79c2ad71ce7cf4990d2b8922bd48c4e7cc88c12a
litlighilit <litlighilit@foxmail.com>
2025-08-17 19:03:06 +0800

    fixup(6edc40902): numTypes/ints not compile if -d:noUndefinedBitOpts

--- src/pylib/numTypes/ints.nim
+++ src/pylib/numTypes/ints.nim
@@ -2,7 +2,8 @@
 import ./ints/[decl, init, getter, int_bytes]
 
 export init.int, init.nimint
-export decl, getter, int_bytes
+export decl, int_bytes
+export getter except bit_lengthUsingBitops
 
 import ./ints/longint
 export longint
--- src/pylib/numTypes/ints/getter.nim
+++ src/pylib/numTypes/ints/getter.nim
@@ -7,10 +7,19 @@ import ../reimporter
 template is_integer*(_: NimInt): bool{.pysince(3,12).} = true
 template as_integer_ratio*(self: NimInt): (NimInt, NimInt){.pysince(3,8).} = (self, 1)
 
+proc bit_lengthUsingBitops*(x: SomeInteger): int =
+  ## inner usage.
+  ##
+  ## undefined result if x == 0
+  ## 
+  ## .. note:: though in ArchLinux with `__GNUC__` == 15 the result for x==0 is 0,
+  ##   you cannot tell this behavior reliable.
+  const BitPerByte = 8
+  sizeof(x) * BitPerByte - bitops.countLeadingZeroBits x
+
 proc bit_length*(self: NimInt): NimInt =
   when defined(noUndefinedBitOpts):
-    const BitPerByte = 8
-    sizeof(x) * BitPerByte bitops.countLeadingZeroBits x
+    bit_lengthUsingBitops self
   else:
     1 + fastLog2 abs(self)
 
