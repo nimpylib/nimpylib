cc0cf693974045ae0a62ad48749eca9cdc9e2fc8
litlighilit <litlighilit@foxmail.com>
2025-12-03 17:54:27 +0800

    impr: genNamedTuple; use toPyNim for namedtuple; ...
    alter `resources.struct_rusage`

--- src/pylib/Lib/resource_impl/types.nim
+++ src/pylib/Lib/resource_impl/types.nim
@@ -1,7 +1,15 @@
 
 import std/posix
+import ../../Objects/structseq
 
-type
+template doubletime(tv: TimeVal): float =
+  float(tv.tv_sec) + float(tv.tv_usec) / 1000000.0
+
+proc toPyNim(tv: TimeVal, res: var float) = res = tv.doubletime
+proc toPyNim(i: clong, res: var int) = res = i
+
+genNamedTuple(RUsage):
+ type
   struct_rusage* = ref object
     ru_utime*: float
     ru_stime*: float
@@ -20,25 +28,7 @@ type
     ru_nvcsw*: int
     ru_nivcsw*: int
 
-template doubletime(tv: TimeVal): float =
-  float(tv.tv_sec) + float(tv.tv_usec) / 1000000.0
 
 proc toPyObject*(rusage: RUsage): struct_rusage =
-  struct_rusage(
-    ru_utime: doubletime(rusage.ru_utime),
-    ru_stime: doubletime(rusage.ru_stime),
-    ru_maxrss: rusage.ru_maxrss,
-    ru_ixrss: rusage.ru_ixrss,
-    ru_idrss: rusage.ru_idrss,
-    ru_isrss: rusage.ru_isrss,
-    ru_minflt: rusage.ru_minflt,
-    ru_majflt: rusage.ru_majflt,
-    ru_nswap: rusage.ru_nswap,
-    ru_inblock: rusage.ru_inblock,
-    ru_oublock: rusage.ru_oublock,
-    ru_msgsnd: rusage.ru_msgsnd,
-    ru_msgrcv: rusage.ru_msgrcv,
-    ru_nsignals: rusage.ru_nsignals,
-    ru_nvcsw: rusage.ru_nvcsw,
-    ru_nivcsw: rusage.ru_nivcsw
-  )
+  rusage.toPyNim result
+
--- src/pylib/Objects/structseq.nim
+++ src/pylib/Objects/structseq.nim
@@ -1,60 +1,126 @@
 ## minic CPython's structseq
 
 import std/macros
+import std/macrocache
+export macros.NimTypeKind, macros.parseExpr
 
-proc mapCTypeToNim(cT: NimNode): NimNode =
-  case cT.typeKind
-  of {ntyInt..ntyInt64}: bindSym"BiggestInt"
-  of {ntyUInt..ntyUInt64}: bindSym"BiggestUInt"
-  of {ntyFloat..ntyFloat64}: bindSym"BiggestFloat"
-  of ntyCString: bindSym"string"
-  else: cT
+type MapCTypeToNim = CacheTable
+using
+  self: MapCTypeToNim
+  key: NimTypeKind
 
-template toNim(i: SomeSignedInt): BiggestInt = i
-template toNim(i: SomeUnsignedInt): BiggestUInt = i
-template toNim(f: SomeFloat): BiggestFloat = f
-template toNim(s: cstring): string = $s
+proc contains*(self; key): bool = self.contains $key
+proc `[]`*(self; key): NimNode = self[$key]
+proc `[]=`*(self; key; val: NimNode) = self[$key] = val
 
-proc cstructGenNamedTupleImpl(cT: NimNode, genName: string, exported: bool): NimNode =
+proc `[]`*(self; key: NimNode): NimNode =
+  let t = key.typeKind
+  if self.contains t: self[t]
+  else: key
+
+template declMapCTypeToNimImpl(mapCTypeToNim; uniqueId: string; additionDo){.dirty.} =
+  ## XXX: (due to Nim's CacheTable lacks `del` method)
+  ##   so this template exists to dup a new one
+  bind CacheTable, `[]`, `[]=`, bindSym
+  bind NimTypeKind
+  const mapCTypeToNim = CacheTable uniqueId
+  static:
+    template m(x: NimTypeKind, v: NimNode) =
+      mapCTypeToNim[x] = v
+    template m(x: NimTypeKind, v: string) =
+      m x, bindSym v
+    template m(x: Slice, v) =
+      for i in x: m i, v
+    m ntyInt..ntyInt64: "BiggestInt"
+    m ntyUInt..ntyUInt64: "BiggestUInt"
+    m ntyFloat..ntyFloat64: "BiggestFloat"
+    m ntyCString: "string"
+    additionDo
+
+macro declMapCTypeToNim*(mapCTypeToNim; uniqueId = astToStr(mapCTypeToNim);
+                            additionalMaps) =
+  result = newCall(bindSym"declMapCTypeToNimImpl", mapCTypeToNim, uniqueId)
+  let lastArg = newStmtList()
+  for kv in additionalMaps:
+    let (k, v) = (kv[0], kv[1])
+    let resV = newLit v.repr
+    lastArg.add quote do: m `k`, parseExpr `resV`
+  result.add lastArg
+
+template declMapCTypeToNim*(mapCTypeToNim; uniqueId = astToStr(mapCTypeToNim)){.dirty.} =
+  declMapCTypeToNimImpl(mapCTypeToNim, uniqueId): discard
+
+declMapCTypeToNim mapCTypeToNim, "structseq.default.mapCTypeToNim"
+
+template directToPyNim(Tsrc, Tdst){.dirty.} =
+  template toPyNim*(i: Tsrc; res: var Tdst) = res = i
+directToPyNim SomeSignedInt, BiggestInt
+directToPyNim SomeUnsignedInt,BiggestUInt
+directToPyNim SomeFloat, BiggestFloat
+template toPyNim*(s: cstring; res: var string) = res = $s
+
+proc oneElement(collection: NimNode, kind: NimNodeKind): NimNode =
+  collection.expectKind kind
+  collection.expectLen 1
+  collection[0]
+
+proc unpackExportedAsStr(node: NimNode): (string, bool) =
+  if node.kind == nnkPostfix and node[0].eqIdent"*":
+    (node[1].strVal, true)
+  else:
+    (node.strVal, false)
+
+proc cstructGenNamedTupleImpl(cT: NimNode,
+    exported: bool, # proc(x: NimNode): NimNode,
+    genName: string, fieldList: NimNode = nil, mapCTypeToNimTab = mapCTypeToNim): NimNode =
   result = newStmtList()
   let nT = ident genName
-  let typImpl = getTypeImpl(cT)
-  assert typImpl.typeKind == ntyObject
-  assert typImpl[0].kind == nnkEmpty
-  assert typImpl[1].kind == nnkEmpty
 
-  var mayExp = if exported:
+  let mayExp = if exported:
     proc (x: NimNode): NimNode = x.postfix"*"
   else:
     proc (x: NimNode): NimNode = x
+  let cTImpl = getTypeImpl(cT)
+  assert cTImpl.typeKind == ntyObject
+  assert cTImpl[0].kind == nnkEmpty
+  assert cTImpl[1].kind == nnkEmpty
+  template fieldsGivenOr(ifExpr, orExpr): untyped =
+    if fieldList.isNil: ifExpr
+    else: orExpr
+
+  let targetFieldList = fieldsGivenOr(cTImpl[2], fieldList)
+  targetFieldList.expectKind nnkRecList
+
   let
     resultId = ident"result"
-    toNimId = bindSym"toNim"
-    toNimDefId = ident"toNim".mayExp
+    toNimId = ident"toPyNim"
+    toNimDefId = ident"toPyNim".mayExp
     dollarId = ident"$".mayExp
     getitemId = ident"[]".mayExp
     cParam = ident"cObj"
     nParam = ident"nObj"
   var
-    toNimBody = newStmtList parseStmt"new result"
+    toNimBody = newStmtList quote do: result = `nT`()
     dollarBody = newStmtList quote do:
       result = `genName`
-  template asgnA(dest, src, attr, attrMap): NimNode{.dirty.} =
-    quote do: `dest`.`attr` = `attrMap` `src`.`attr`
+
+  template asgnToNim(dest, src, attr): NimNode{.dirty.} =
+    quote do: `toNimId`(`src`.`attr`, `dest`.`attr`)
 
   let emptyn = newEmptyNode()
   var fields = newNimNode nnkRecList
   var fieldListValue = newNimNode nnkBracket
   var preStr = "("
-  for oriFDef in typImpl[2]:
+  for oriFDef in targetFieldList:
     # oriFDef.kind == nnkIdentDefs
     let fDef = oriFDef.copyNimNode
     for idxInIdentDef in 0..<oriFDef.len-2:
-      let fStrVal = oriFDef[idxInIdentDef].strVal
+      let (fieldName, _) = oriFDef[idxInIdentDef].unpackExportedAsStr
+      let fStrVal = fieldName
       let fStr = newLit fStrVal
       let f = ident fStrVal  # purge symbol type info
       fDef.add f.mayExp
-      toNimBody.add asgnA(resultId, cParam, f, toNimId)
+      toNimBody.add asgnToNim(resultId, cParam, f)
       fieldListValue.add fStr
       dollarBody.add quote do:
         result.add `preStr`
@@ -63,23 +129,40 @@ proc cstructGenNamedTupleImpl(cT: NimNode, genName: string, exported: bool): Nim
         result.add repr `nParam`.`f`
 
       preStr = ", "
-    fDef.add mapCTypeToNim oriFDef[^2]
+    var targetType = oriFDef[^2]
+    fDef.add fieldsGivenOr(mapCTypeToNimTab[targetType], targetType)
     fDef.add oriFDef.last
     fields.add fDef
   dollarBody.add quote do: result.add ')'
 
-  let objFields = nnkRefTy.newTree nnkObjectTy.newTree(emptyn, emptyn, fields)
+  let nTobjDef = nnkRefTy.newTree nnkObjectTy.newTree(emptyn, emptyn, fields)
+  let nTypeDef = quote do:
+    type `nT`* = `nTobjDef`
+  result.add nTypeDef
   result.add quote do:
-    type `nT`* = `objFields`
-  result.add quote do:
-    proc `toNimDefId`(`cParam`: sink `cT`): `nT` = `toNimBody`
-    proc `toNimDefId`(`cParam`: ptr`cT`): `nT`{.inline.} = `cParam`[].toNim
+    proc `toNimDefId`(`cParam`: sink `cT`; `resultId`: var `nT`) = `toNimBody`
+    proc `toNimDefId`(`cParam`: ptr`cT`; `resultId`: var `nT`){.inline.} = `cParam`[].`toNimId` `resultId`
     proc `dollarId`(`nParam`: `nT`): string = `dollarBody`
     macro `getitemId`(`nParam`: `nT`, getitemIdx: static[int]): untyped =
       const fieldList = `fieldListValue`
       var idx = getitemIdx
       if idx < 0: idx = fieldList.len - idx
       newDotExpr(`nparam`, ident fieldList[idx])
+
+proc genNamedTupleImpl(cT: NimNode, stmtDef: NimNode): NimNode =
+  let typSec = stmtDef.oneElement(nnkStmtList)
+  let typDef = typSec.oneElement(nnkTypeSection)
+  assert typDef.kind == nnkTypeDef
+  let (name, exported) = typDef[0].unpackExportedAsStr
+
+  let objRefTy = typDef[2]
+  assert objRefTy.kind == nnkRefTy # TODO: allow object
+  let objTy = objRefTy[0]
+  cstructGenNamedTupleImpl(cT, exported, name, objTy[2])
+
+macro genNamedTuple*[T: object](desc: typedesc[T], stmtDef) =
+  let cT = desc.getType[1]
+  genNamedTupleImpl(cT, stmtDef)
 
 macro cstructGenNamedTuple*[T: object](desc: typedesc[T], genName: static[string], exported: static[bool] = true) =
   ##[
@@ -90,19 +173,21 @@ macro cstructGenNamedTuple*[T: object](desc: typedesc[T], genName: static[string
   ]##
 
   let cT = desc.getType[1]
-  cstructGenNamedTupleImpl(cT, genName, exported)
+  cstructGenNamedTupleImpl(cT, exported, genName)
 
-macro cstructGenNamedTuple*[T: object](desc: typedesc[T], exported: static[bool] = true) =
-  ## Generate with name "struct_" prefix
+macro cstructGenNamedTuple*[T: object](desc: typedesc[T], exported: static[bool] = true;
+                                       mapCTypeToNim: static[MapCTypeToNim]=mapCTypeToNim) =
+  ## Generate with name "struct_" prefix.
   let cT = desc.getType[1]
   let nTStr = "struct_" & cT.strVal
-  cstructGenNamedTupleImpl(cT, nTStr, exported)
+  cstructGenNamedTupleImpl(cT, exported, nTStr, mapCTypeToNimTab=mapCTypeToNim)
 
 when isMainModule and defined(posix):
   import std/posix
   cstructGenNamedTuple Passwd
   let c = getpwnam"root"
-  let n = c.toNim
+  var n: struct_passwd
+  c.toPyNim n
   echo n
   echo n[0]
 
